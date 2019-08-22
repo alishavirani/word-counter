ef64362c22837d85e565e37c556a99fd595b83ad
Alisha Virani <alisha.virani@accionlabs.com>
2019-08-22 18:19:11 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+let minutes, seconds = 0;
+
+function startTimer() {
+    const timer = +document.getElementById("timer").value || 0;
+
+    if (timer > 0) {
+        document.getElementById('button').disabled = true;
+        document.getElementById('timer').disabled = true;
+        document.getElementById('textarea').disabled = false;
+        document.getElementById('timeLeft').style.display = "block";
+    }
+
+    let date = new Date();
+
+    date.setMinutes(date.getMinutes() + timer);
+    const countDownTime = date.getTime();
+
+    const timeInterval = setInterval(() => {
+        const now = new Date().getTime();
+        const diff = countDownTime - now;
+
+        minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        seconds = Math.floor((diff % (1000 * 60)) / 1000);
+        document.getElementById("time").innerHTML = minutes + "m " + seconds + "s ";
+
+        if (diff < 0) {
+            clearInterval(timeInterval);
+            document.getElementById("time").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+
+    if (document.getElementById("time").innerHTML === "EXPIRED") {
+        console.log("whyyyy")
+        document.getElementById('button').disabled = false;
+        document.getElementById('timer').disabled = false;
+    }
+}
+
+const input = document.querySelectorAll('textarea')[0];
+input.addEventListener('keyup', () => {
+    const words = input.value.match(/\b[-?(\w+)?]+\b/gi);
+    if (words) {
+        document.getElementById("count").innerHTML = words.length;
+    } else {
+        document.getElementById("count").innerHTML = 0;
+    }
+});
